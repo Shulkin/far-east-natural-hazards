59792cecbbace55ec25a4262adf48c24eaf85504
Evgeny Shulkin <evgeny.shulkin@gmail.com>
2017-05-30 16:19:44 +1000

    Fix bug in home route

--- src/app/app.js
+++ src/app/app.js
@@ -4,4 +4,4 @@ import uirouter from "angular-ui-router";
 import routing from "./app.config.js";
 // my modules
 import home from "./home/home" // home.js from home folder
-angular.module("app", []).config(routing);
+angular.module("app", [uirouter, home]).config(routing);
--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ var config = {
     rules: [{ // allows to use ES 2015 features
       test: /\.js$/,
       // don't parse node_modules and vendor libraries
-      exclude: [/node_modules/, path.resolve(__dirname, "src/vendor")]
+      exclude: [/node_modules/, path.resolve(__dirname, "src/vendor")],
       use: {
         loader: "babel-loader",
         options: {presets: ["es2015"]}
@@ -30,9 +30,17 @@ var config = {
       use: ["style-loader", "css-loader"]
     }, { // copy static html and favicon
       test: /\.(html|ico)$/,
-      // Only from source folder root
-      include: [path.resolve(__dirname, "src")],
+      // maybe unnecessary to write rules only for two files?
+      include: [
+        path.resolve(__dirname, "src/index.html"),
+        path.resolve(__dirname, "src/favicon.ico")
+      ],
+      // nevertheless we just need to copy them
       use: ["file-loader?name=[path][name].[ext]"]
+    }, { // any other html process as raw text for angular templates
+      test: /\.(html|txt)$/,
+      exclude: [path.resolve(__dirname, "src/index.html")], // just in case
+      use: ["raw-loader"]
     }]
   },
   plugins: [
