03262bbcc1fe56c5c9cd2952a9ca61396f51042a
Evgeny Shulkin <evgeny.shulkin@gmail.com>
2017-06-02 16:04:24 +1000

    Test run bulma styles

--- src/app/main.js
+++ src/app/main.js
@@ -4,5 +4,7 @@ import "../index.html";
 import "../favicon.ico";
 // require styles
 import "./common/common.style.scss";
+// require bulma
+import "bulma";
 // require app script
 import "./app.js";
