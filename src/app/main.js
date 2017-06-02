0de4917f44ab111c4328eabefe8beefdacea606b
Evgeny Shulkin <evgeny.shulkin@gmail.com>
2017-06-02 16:17:18 +1000

    Showcase some bulma features

--- src/app/main.js
+++ src/app/main.js
@@ -4,7 +4,7 @@ import "../index.html";
 import "../favicon.ico";
 // require styles
 import "./common/common.style.scss";
-// require bulma
+// require bulma css framework
 import "bulma";
 // require app script
 import "./app.js";
