3767f975b8686b14c63b6066eba6967af22555e3
Evgeny Shulkin <evgeny.shulkin@gmail.com>
2017-06-06 16:21:16 +1000

    Import ol directly

--- src/app/app.js
+++ src/app/app.js
@@ -4,4 +4,6 @@ import uirouter from "angular-ui-router";
 import routing from "./app.config.js";
 // my modules
 import home from "./home/home" // home.js from home folder
+import ol from "../vendor/ol3/ol-custom.js";
+console.log(ol);
 angular.module("app", [uirouter, home]).config(routing);
