4906e01a2a0408baee4a6e578d7eadc5bc0a98c6
Evgeny Shulkin <evgeny.shulkin@gmail.com>
2017-06-06 16:10:56 +1000

    Add map init

--- src/app/home/home.controller.js
+++ src/app/home/home.controller.js
@@ -3,5 +3,17 @@ export default class HomeController {
     this.copyright = "Evgeny Shulkin";
     this.license = "https://opensource.org/licenses/GPL-3.0";
     this.github = "https://github.com/Shulkin/far-east-natural-hazards";
+    var map = new ol.Map({
+      target: "map",
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.OSM()
+        })
+      ],
+      view: new ol.View({
+        center: [0, 0],
+        zoom: 4
+      })
+    });
   }
 }
